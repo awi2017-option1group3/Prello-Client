74d33b46195597f8dbe13a5843019d830a50b87a
RINGAYEN David <david.ringayen@icloud.com>
2017-11-02 19:40:34 +0100

    Add Google Drive picker
    Add the google drive picker and configurate the googlde project

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import GooglePicker from 'react-google-picker'
 import './style.css'
 import { Layout, Button } from 'antd'
 
 const { Sider, Content } = Layout
+
+const SCOPE = ['https://www.googleapis.com/auth/drive.readonly']
 
 class CardDetails extends Component {
   render() {
@@ -19,7 +22,19 @@ class CardDetails extends Component {
             <Sider className="sider">
               <Button type="primary" className="siderButton">Button Members</Button>
               <Button type="primary" className="siderButton">Button labels</Button>
-              <Button type="primary" className="siderButton">Button drive</Button>
+              <GooglePicker
+                clientId={process.env.REACT_APP_DRIVE_CLIENT_ID}
+                developerKey={process.env.REACT_APP_DRIVE_DEVELOPER_KEY}
+                scope={SCOPE}
+                onChange={data => console.log('on change:', data)}
+                multiselect
+                navHidden
+                authImmediate={false}
+                mimeTypes={['image/png', 'image/jpeg', 'image/jpg']}
+                viewId={'DOCS'}
+              >
+                <Button type="primary" className="siderButton">Google Drive</Button>
+              </GooglePicker>
             </Sider>
           </Layout>
         </Layout>
